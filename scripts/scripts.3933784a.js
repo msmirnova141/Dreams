a2a9fd4fff82b7961ae804272d9eaa8744b33d69
Marya Smirnova <msmirnov@sfu.ca>
2015-04-06 22:54:16 -0700

    changes

--- scripts/scripts.3933784a.js
+++ scripts/scripts.3933784a.js
@@ -234,14 +234,13 @@ angular.module('dreamsApp')
 
 
 angular.module('dreamsApp')
-  .controller('RecallController', ['$scope', '$location', function ($scope, $location, $http) {
+  .controller('RecallController', ['$scope', '$location', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    $scope.step = 1;
    $scope.items = '';
    $scope.name = '';
     var test = function(arr){
@@ -255,7 +254,7 @@ angular.module('dreamsApp')
       return newTags;
     };
 
-   $scope.tags = []; 
+   $scope.tags = ['cat']; 
 
    $scope.test = function(arr){
      $scope.newTags = [];
@@ -423,3 +422,36 @@ angular.module('ngGiphy', [])
     return output;
 
   }]);
+
+(function($) {
+	$.fn.tilt = function(params) {
+		items = this;
+		
+		params = $.extend( {sensitivity: 1}, params);
+		
+		ax = ay = 0;
+		
+		window.addEventListener('devicemotion', function (e) {
+			ax = e.accelerationIncludingGravity.x * params.sensitivity;
+			ay = -e.accelerationIncludingGravity.y * params.sensitivity;
+			
+			if(ax > 0) {
+				ax -= params.sensitivity;
+				if(ax < 0) ax = 0;
+			} else if(ax < 0) {
+				ax += params.sensitivity;
+				if(ax > 0) ax = 0;
+			}
+			
+		}, false);
+		
+		mainLoop = setInterval("moveMe()");
+		
+		moveMe = function() {
+			$(items).each(function() {
+				scrollPos = $(this).scrollLeft() + ax;
+				$(this).scrollLeft(scrollPos);
+			});
+		}
+	}
+})(jQuery);
